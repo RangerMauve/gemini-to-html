35ae54dd40e0c80d8bbff6e37de268e05efcb291
Mauve Signweaver <RangerMauve@hotmail.com>
2024-03-13 14:32:26 -0400

    wrap links in paragraphs instead of divs

--- render.js
+++ render.js
@@ -16,7 +16,7 @@ function render (tokens) {
     switch (type) {
       case 'quote': return htmlEscape`<blockquote>${line.content}</blockquote>`
       case 'header': return htmlEscape`<h${line.level} id="${id(line.content)}">${line.content}</h${line.level}>`
-      case 'link': return htmlEscape`<div><a href="${line.href}">${line.content}</a></div>`
+      case 'link': return htmlEscape`<p><a href="${line.href}">${line.content}</a></p>`
       case 'pre': return line.alt
         ? htmlEscape`<pre><code class="language-${line.alt}">\n${line.items.join('\n')}\n</code></pre>`
         : htmlEscape`<pre>\n${line.items.join('\n')}\n</pre>`
